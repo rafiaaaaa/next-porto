02358b738b3e9de63fe31d246f4075d308c4f446
rafia <akmalrafi1132@gmail.com>
2026-02-04 15:23:03 +0700

    polish: border profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,10 +78,10 @@ export default function Home() {
     <Section>
       <div className="space-y-6">
         <div className="flex flex-col gap-2 md:gap-0 md:flex-row-reverse">
-          <div className="flex-1 flex justify-center items-center relative overflow-hidden">
-            <div className="w-52 h-52 md:w-72 md:h-72 rounded-full border-4 border-double p-2 border-purple-950 border-l-0 border-r-0 animate-spin rotate-12">
-              <div className="w-full h-full rounded-full border-8 border-double p-2 border-slate-950 border-t-2 border-b-2 rotate-45">
-                <div className="w-full h-full rounded-full border-8 border-purple-950 border-t-0 border-b-0 rotate-6" />
+          <div className="flex-1 flex justify-center items-center relative overflow-visible">
+            <div className="w-52 h-52 md:w-72 md:h-72 rounded-full border-8 border-double  border-purple-400 animate-spin bg-slate-950 border-l-0 border-r-0  rotate-12 p-1 shadow-[0_0_10px_#000000,0_0_80px_#650192,0_0_40px_#000000] ">
+              <div className="w-full h-full rounded-full border-8 border-double p-2 border-purple-600 border-t-2 border-b-2 rotate-45">
+                <div className="w-full h-full rounded-full border-8 border-white border-t-0 border-b-0 rotate-6" />
               </div>
             </div>
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-4 overflow-hidden rounded-full w-40 h-40 md:w-60 md:h-60 mx-auto">
@@ -94,9 +94,10 @@ export default function Home() {
               />
             </div>
           </div>
-          <div className="text-center md:text-left flex-1 flex flex-col justify-center relative overflow-visible">
+          <div className="text-center mt-10 md:mt-0 md:text-left flex-1 flex flex-col justify-center relative overflow-visible">
             <h1 className="text-2xl md:text-3xl font-bold tracking-tight">
-              Hi, I’m Rafi Akmal 👋
+              Hi, I’m Rafi Akmal{" "}
+              <div className="animate-bounce inline-block">👋</div>
             </h1>
             <p className="mt-2 text-muted-foreground tracking-tighter">
               I’m a <span className="font-bold">Full Stack Developer</span>{" "}
@@ -118,7 +119,7 @@ export default function Home() {
                       `px-3 shadow-xl text-white cursor-pointer transition-all duration-75 hover:scale-110 tracking-wide flex items-center justify-center gap-2 md:py-1`,
                       style.bg,
                       style.text,
-                      style.shadow
+                      style.shadow,
                     )}
                   >
                     <span className="flex items-center justify-center w-5 h-5 bg-white/80 dark:bg-black/40 rounded-full overflow-hidden">
